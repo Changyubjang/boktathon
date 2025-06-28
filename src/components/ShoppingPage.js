34e4bdba02ec33719d59067b45a747d798b7b2ae
YunseoChoi0919 <144880431+YunseoChoi0919@users.noreply.github.com>
2025-06-28 15:07:47 +0900

    윤서

--- src/components/ShoppingPage.js
+++ src/components/ShoppingPage.js
@@ -18,20 +18,20 @@ const ShoppingPage = () => {
       name: '임영웅', 
       image: `${임영웅}`,
       products: [
-        { id: 1, name: '임영웅 공식 포스터', price: '15,000원', image: 'ㅣㅣㅣ' },
-        { id: 2, name: '임영웅 트로트 앨범 CD', price: '25,000원', image: 'https://via.placeholder.com/150x200/FF8E8E/white?text=CD' },
-        { id: 3, name: '임영웅 팬클럽 키트', price: '35,000원', image: 'https://via.placeholder.com/150x200/FFB3B3/white?text=KIT' },
-        { id: 4, name: '임영웅 티셔츠', price: '45,000원', image: 'https://via.placeholder.com/150x200/FFD8D8/white?text=TSHIRT' },
+        { id: 1, name: '임영웅 공식 포스터', price: '15,000원', image: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDA4MjlfMTI5%2FMDAxNzI0ODY0MDIwNzc4.T-b4OkbmcyH-df9ad9pbvMP_6K-LwGDBKP-ed4fcJ90g.WpvkRDY3dAHdoufLXMHVcLgtmtS2tGkbXgdeBi5wG5Ug.JPEG%2FIMG_3020.jpg&type=a340' },
+        { id: 2, name: '임영웅 트로트 앨범 CD', price: '25,000원', image: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F421%2F2022%2F04%2F19%2F0006037088_001_20220419090301660.jpg&type=a340' },
+        { id: 3, name: '임영웅 응원봉', price: '35,000원', image: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDA4MDJfMTc3%2FMDAxNzIyNTQ4NzU4MDM2.MzgfCRo89FZ-vhaIKsDO4mj8FLQdm9Kf7fP5Mienokog.HFWNyRqpzZHpARlf95GkkfId_3EnoSRHtZowF1m1A04g.PNG%2Fimage.png&type=a340' },
+        { id: 4, name: '임영웅 티셔츠', price: '45,000원', image: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fshop1.phinf.naver.net%2F20230921_106%2F1695277151935Y0XR9_JPEG%2F13580583759041592_467270312.jpg&type=a340' },
       ]
     },
     2: { 
       name: '김호중', 
       image: `${김호중}`,
       products: [
-        { id: 1, name: '김호중 공식 포스터', price: '15,000원', image: 'https://via.placeholder.com/150x200/4ECDC4/white?text=POSTER' },
-        { id: 2, name: '김호중 트로트 앨범 CD', price: '25,000원', image: 'https://via.placeholder.com/150x200/6EE7DF/white?text=CD' },
-        { id: 3, name: '김호중 응원봉', price: '35,000원', image: 'https://via.placeholder.com/150x200/8EF1E9/white?text=KIT' },
-        { id: 4, name: '김호중 트로트 티셔츠', price: '45,000원', image: 'https://via.placeholder.com/150x200/AEFBF3/white?text=TSHIRT' },
+        { id: 1, name: '김호중 공식 포스터', price: '15,000원', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_oYLZjkNi2tMes3R6a1iNaX-YFZB_Jth7Ng&s' },
+        { id: 2, name: '김호중 트로트 앨범 CD', price: '25,000원', image: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTA4MjFfMTM4%2FMDAxNjI5NTIxMTA5NTE0.edJuXxkuhHsXfsBZVov4MiACWJofua5iEn-wQ2CwgpUg.HOe1euey7zDuCCV5VXNv2bnGGYiTamxZI5u2Kf6bFHcg.JPEG.jangha0806%2FScreenshot%25A3%25DF20210821%25A3%25AD122103%25A3%25DFNAVER.jpg&type=a340' },
+        { id: 3, name: '김호중 응원봉', price: '35,000원', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlLqTIwhmYmbwKiw-sA5nBJPBU4QSZ-g34Bw&s' },
+        { id: 4, name: '김호중 티셔츠', price: '45,000원', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoMGWMAXwccB08OcYujvZgv3tsVb_XvraFVg&s' },
       ]
     },
     3: { 
@@ -48,20 +48,20 @@ const ShoppingPage = () => {
       name: '영탁', 
       image: `${영탁}`,
       products: [
-        { id: 1, name: '영탁 공식 포스터', price: '15,000원', image: 'https://via.placeholder.com/150x200/96CEB4/white?text=POSTER' },
-        { id: 2, name: '영탁 트로트 앨범 CD', price: '25,000원', image: 'https://via.placeholder.com/150x200/B8E0C6/white?text=CD' },
-        { id: 3, name: '영탁 응원봉', price: '35,000원', image: 'https://via.placeholder.com/150x200/DAF2D8/white?text=KIT' },
-        { id: 4, name: '영탁 티셔츠', price: '45,000원', image: 'https://via.placeholder.com/150x200/FCF4EA/white?text=TSHIRT' },
+        { id: 1, name: '영탁 공식 포스터', price: '15,000원', image: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjA2MjFfMjEy%2FMDAxNjU1ODE5ODEzMDY0.s51FNR67fYNe2MCuT2pM3RjJJpvS7qRQbmUkI_CnW9Mg.WGzdfZYeHq9cyotVBNEcXOkhXd6W3atU9d-Qs2KgO8og.JPEG.dhsmfdntwk2020%2F%25C5%25B9%25BC%25EE%25C6%25F7%25BD%25BA%25C5%25CD.jpg&type=a340' },
+        { id: 2, name: '영탁 트로트 앨범 CD', price: '25,000원', image: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzA5MDRfMjM2%2FMDAxNjkzNzk3MDYzMDE3.OZ6HDuNIEEmmRNuhts2c8DW-Rnveec9A1cKrHCWW-kkg.ZKC8Ao2NiaL8-atiJY3jpBdeMmWwc19GcJS-csjf4K4g.JPEG.hee2318%2FScreenshot%25A3%25DF20230904%25A3%25DF120552%25A3%25DFKakaoTalk.jpg&type=a340' },
+        { id: 3, name: '영탁 응원봉', price: '35,000원', image: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzA4MTZfMjMy%2FMDAxNjkyMTczNjk3NjYx.tulYAqSt71Duk8FwenQhYbVwB6rHnq78MJc4It70KXQg.5FkewuWJWJFwX5X6-PjZ99CJdbD7oPD5I7SExk0WCGMg.JPEG.hyetak2%2F20230816%25A3%25DF170549.jpg&type=a340' },
+        { id: 4, name: '영탁 티셔츠', price: '45,000원', image: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fshop1.phinf.naver.net%2F20210701_62%2F1625108105011dEa4U_JPEG%2F26244003718662662_1678300791.jpg&type=a340' },
       ]
     },
     5: { 
       name: '이찬원', 
       image: `${이찬원}`,
       products: [
-        { id: 1, name: '이찬원 공식 포스터', price: '15,000원', image: 'https://via.placeholder.com/150x200/FFA726/white?text=POSTER' },
-        { id: 2, name: '이찬원 트로트 앨범 CD', price: '25,000원', image: 'https://via.placeholder.com/150x200/FFB74D/white?text=CD' },
-        { id: 3, name: '이찬원 응원봉', price: '35,000원', image: 'https://via.placeholder.com/150x200/FFCC80/white?text=KIT' },
-        { id: 4, name: '이찬원 티셔츠', price: '45,000원', image: 'https://via.placeholder.com/150x200/FFE0B2/white?text=TSHIRT' },
+        { id: 1, name: '이찬원 공식 포스터', price: '15,000원', image: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTA5MjRfMTg4%2FMDAxNjMyNDgyOTc3NTg4.JmVqsxKrmyRJ2RxFDtEa9bNdR1OaLA9wkZ08-FPNZQAg.8FWVMZQPTWNy_6m7VhweGB3p4jzwgf5lddCTCG3MzOQg.JPEG.dotorymin0910%2F%25C0%25CC%25C2%25F9%25BF%25F8%25A3%25DF%25C4%25DC%25BC%25AD%25C6%25AE%25A3%25DF%25C4%25AB%25C6%25E4%25A3%25DF%25B0%25F8%25C1%25F6%25A3%25DF%25C3%25D6%25C1%25BE.jpg&type=a340' },
+        { id: 2, name: '이찬원 트로트 앨범 CD', price: '25,000원', image: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzEwMjJfODEg%2FMDAxNjk3OTQyOTk5MzU2.a6u6m-zfTpHm1oyMTaaOTIyTeQ36BaF69Wmj6ebyLJcg.wVbhUBnP5fIeUwtwaHXCMq9t7aIYlZu60r8fe8N1fPAg.JPEG.whdmsgml235%2F1697942936073.jpg&type=a340' },
+        { id: 3, name: '이찬원 응원봉', price: '35,000원', image: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fshop1.phinf.naver.net%2F20240510_227%2F1715310604854thbB2_PNG%2F42613288734406462_1138341572.png&type=a340' },
+        { id: 4, name: '이찬원 티셔츠', price: '45,000원', image: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fshop1.phinf.naver.net%2F20240528_37%2F1716906905187zt60p_JPEG%2F118042803885339378_439756402.jpg&type=a340' },
       ]
     },
   };
